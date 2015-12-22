99e4fdfcdd00376afbea21b11d75bb961ec9b246
ahfoss <alexanderhfoss@gmail.com>
2015-12-22 01:26:37 -0500

    Inserted new MEDEA/FAMD methods

--- R/medea.R
+++ R/medea.R
@@ -33,6 +33,78 @@ setArgs <- function(input, name, default) {
 }
 
 
+# helper function for medea with famd option
+propVar <- function(X1,X2) {
+  if (is.numeric(X1) && is.numeric(X2)) {
+    depVar <- scale(X1)
+    indVar <- scale(X2)
+    return(summary(lm(depVar ~ indVar))$r.squared)
+  }
+  if (is.numeric(X1) && is.factor(X2)) {
+    depVar <- scale(X1)
+    indVar <- model.matrix(~ 0 + X2)
+    return(summary(lm(depVar ~ indVar))$r.squared)
+  }
+  if (is.factor(X1) && is.numeric(X2)) {
+    depVar <- scale(X2)
+    indVar <- model.matrix(~ 0 + X1)
+    return(summary(lm(depVar ~ indVar))$r.squared)
+  }
+  if (is.factor(X1) && is.factor(X2)) {
+    X1d <- model.matrix(~ 0 + X1)
+    X2d <- model.matrix(~ 0 + X2)
+    if (ncol(X1d) < 2 || ncol(X2d) < 2) stop('Factor must have more than one level')
+
+    # original method
+    #H1 <- X1d %*% solve(t(X1d) %*% X1d) %*% t(X1d)
+    #H2 <- X2d %*% solve(t(X2d) %*% X2d) %*% t(X2d)
+    #nn <- nrow(X1d)
+    #ninv <- 1/nn
+    #Jmat <- matrix(ninv, nrow=nn, ncol=nn)
+    #ratio1 <- sum(diag(t(X1d) %*% (H2-Jmat) %*% X1d)) / sum(diag(t(X1d) %*% (diag(nn)-Jmat) %*% X1d))
+    #ratio2 <- sum(diag(t(X2d) %*% (H1-Jmat) %*% X2d)) / sum(diag(t(X2d) %*% (diag(nn)-Jmat) %*% X2d))
+
+    # faster method
+    ssr1 <- 0
+    sst1 <- 0
+    for (i in 1:ncol(X1d)) {
+      res1 <- lm(X1d[,i] ~ X2d)
+      aov1 <- anova(res1)
+      sss1 <- aov1$`Sum Sq`
+      ssr1 <- ssr1 + sum(sss1[-length(sss1)])
+      sst1 <- sst1 + sum(sss1)
+    }
+    rSq1 <- ssr1 / sst1
+
+    ssr2 <- 0
+    sst2 <- 0
+    for (i in 1:ncol(X2d)) {
+      res2 <- lm(X2d[,i] ~ X1d)
+      aov2 <- anova(res2)
+      sss2 <- aov2$`Sum Sq`
+      ssr2 <- ssr2 + sum(sss2[-length(sss2)])
+      sst2 <- sst2 + sum(sss2)
+    }
+    rSq2 <- ssr2 / sst2
+
+    return(rSq1/2 + rSq2/2)
+  } 
+  stop('Unrecognized input types.')
+}
+
+# helper function for medea with famd option
+propVarAssocMat <- function(dat) {
+  pp <- ncol(dat)
+  outMat <- matrix(0,nrow=pp,ncol=pp)
+  for (i in 1:(pp-1)) {
+    for (j in (i+1):pp) {
+      outMat[i,j] <- propVar(dat[,i],dat[,j])
+    }
+  }
+  return(outMat + t(outMat))
+}
+
+
 # Options: ari+kmeans, ari+quantile, or famd
 
 #' MEDEA weight calculation
@@ -43,7 +115,8 @@ setArgs <- function(input, name, default) {
 #' @param associationMethod The method for calculating the association matrix.
 #' @param verbosity Whether to include extended results objects.
 #' @param ... Optional input to the discretization function.
-#' @return A vector of variable-specific weights with length equal to \code{ncol(dat)}.
+#' @return A list containing a vector \code{wgts} of variable-specific weights with
+#' length equal to \code{ncol(dat)}.
 medeaWgts <- function(
   dat,
   discretizationMethod='kmeans',
@@ -109,10 +182,13 @@ medeaWgts <- function(
     }
     return(results)
   } else if (associationMethod == 'famd') {
-    # generate matrix using multiple correspondence analysis
-    famdRes <- FAMD(dat, ncp=ncol(dat), graph=FALSE)$var$contrib
-    wgts <- famdRes[,1]
-    wgts <- wgts / sum(wgts)
+    assocMat <- propVarAssocMat(dat)
+    # note the association matrix already has zeros
+    # along the main diagonal as required by medea
+    eigs <- eigen(assocMat)
+    wgts <- abs(eigs$vectors[,1])
     results <- list(wgts=wgts)
+    return(results)
   }
 }
+
