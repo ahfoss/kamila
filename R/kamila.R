2ddf971c7eea179a0007e1dc7a9f19d4ee95b8cd
ahfoss <alexanderhfoss@gmail.com>
2017-03-20 11:28:20 -0400

    Add documentation to kamilaClassify

--- R/kamila.R
+++ R/kamila.R
@@ -876,6 +876,22 @@ cyclicalCoding <- function(invar) {
 #' @param obj An output object from the kamila function.
 #' @param newData A list of length 2, with first element a data frame of continuous variables, and second element a data frame of categorical factors.
 #' @return An integer vector denoting cluster assignments of the new data points.
+#' @examples
+#' # Generate toy data set
+#' set.seed(1234)
+#' dat1 <- genMixedData(400, nConVar = 2, nCatVar = 2, nCatLevels = 4, nConWithErr = 2, nCatWithErr = 2, popProportions = c(.5,.5), conErrLev = 0.2, catErrLev = 0.2)
+#' # Partition the data into training/test set
+#' trainingIds <- sample(nrow(dat1$conVars), size = 300, replace = FALSE)
+#' catTrain <- data.frame(apply(dat1$catVars[trainingIds,], 2, factor))
+#' conTrain <- data.frame(scale(dat1$conVars)[trainingIds,])
+#' catTest <- data.frame(apply(dat1$catVars[-trainingIds,], 2, factor))
+#' conTest <- data.frame(scale(dat1$conVars)[-trainingIds,])
+#' # Run the kamila clustering procedure on the training set
+#' kamilaObj <- kamila(conTrain, catTrain, numClust = 2, numInit = 10)
+#' table(dat1$trueID[trainingIds], kamilaObj$finalMemb)
+#' # Predict membership in the test data set
+#' kamilaPred <- classifyKamila(kamilaObj, list(conTest, catTest))
+#' table(dat1$trueID[-trainingIds], kamilaPred)
 classifyKamila <- function(obj,newData) {
   #if (length(newData) == 3) {
   #  cyclicRecoded <- as.data.frame(lapply(newData[[3]],cyclicalCoding))
